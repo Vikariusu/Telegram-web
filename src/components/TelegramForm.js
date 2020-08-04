f5945024bf441af06114a18778b443e27aa57e31
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-08-04 14:10:11 -0400

    feat: add basic routing, a form to submit a telegram and stripe checkout

---
+++ src/components/TelegramForm.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { toast } from 'react-toastify';
+
+toast.configure();
+
+class TelegramForm extends Component {
+    state = {
+        message: ''
+    }
+
+    onInputChange = (e) => {
+        const value = e.target.value;
+        const inputName = e.target.name;
+        this.setState(() => ({ [inputName]: value }));
+    }
+
+    // handleSubmit = async () => {
+    //     // handle submitting the form
+    //     const url = "http://localhost:3000/telegrams";
+
+    //     const rawResponse = await fetch(url, {
+    //         method: "POST",
+    //         headers: {
+    //         Accept: "application/json",
+    //         "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(this.state),
+    //     });
+    //     const content = await rawResponse.json();
+
+    //     console.log(content);
+    // }
+
+    handleToken = async (token) => {
+        const product = {
+            name: 'Telegram',
+            price: 2.5,
+            description: 'a telegram'
+        };
+        // new
+        const telegram = {
+          message: this.state.message
+        }
+
+        // telegram added
+        const body = { token, product, telegram };
+        console.log(body)
+        const url = "http://localhost:3000/checkout";
+
+        const rawResponse = await fetch(url, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+        const content = await rawResponse.json();
+
+        const { status } = content;
+
+        if (status === 'success') {
+            toast('Success! Check email for details',
+            { type: 'success'})
+        } else {
+            toast('Something went wrong', {
+                type: 'error'
+            })
+        }
+    }
+
+    render() {
+        return (
+          <div>
+            <form>
+              <label htmlFor="message">Message</label>
+              <textarea onChange={this.onInputChange} name="message" />
+            </form>
+              <StripeCheckout
+                stripeKey="pk_test_JVMoB5wKqBGDEaxya5HSd0gg00zDywCZMr"
+                token={this.handleToken}
+                billingAddress
+                shippingAddress
+                amount={250}
+                name="telegram"
+              />
+          </div>
+        );
+    }
+}
+
+export default TelegramForm;
